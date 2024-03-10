b0bb1dda224a56351df1ba7bc200e2b78b86148e
Dak <dakgrant2016@gmail,com>
2024-03-10 22:55:32 +0000

    Well i didn't make it work using only css but i did get rid of the double menu press bug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function ShowNav(){
   var x = document.getElementById("nav");
-  if (x.style.display === "none") {
+  if (x.style.display != "flex") {
     x.style.display = "flex";
   } else {
     x.style.display = "none";
