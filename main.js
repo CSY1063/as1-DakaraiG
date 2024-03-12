47ba1a854a1e68555d76e4bc507d45d93f5c697e
Dak <dakgrant2016@gmail,com>
2024-03-12 19:51:43 +0000

    its not perfect but it will have to do

--- main.js
+++ main.js
@@ -6,3 +6,48 @@ function ShowNav(){
     x.style.display = "none";
   }
 }
+
+
+  var check = check + 1
+
+window.onload = () => {
+   var headTag = document.getElementsByTagName('body')[0]
+  const linkforCSSfile = document.createElement("link");
+  if (check > 1) {
+    linkforCSSfile.href = z
+ } else {
+   linkforCSSfile.href = "dark.css"
+  }
+  linkforCSSfile.type = 'text/css'
+  linkforCSSfile.rel = 'stylesheet'
+  headTag.appendChild(linkforCSSfile);
+  document.body.appendChild(headTag);
+   var check = check + 1
+ };
+
+
+
+
+light = () => {
+  var headTag = document.getElementsByTagName('body')[0]
+  const linkforCSSfile = document.createElement("link");
+  linkforCSSfile.href = 'light.css'
+  linkforCSSfile.type = 'text/css'
+  linkforCSSfile.rel = 'stylesheet'
+  headTag.appendChild(linkforCSSfile);
+  document.body.appendChild(headTag);
+  var z = "light.css"
+  var check = check + 1
+};
+
+dark = () => {
+  var headTag = document.getElementsByTagName('body')[0]
+  const linkforCSSfile = document.createElement("link");
+  linkforCSSfile.href = 'dark.css'
+  linkforCSSfile.type = 'text/css'
+  linkforCSSfile.rel = 'stylesheet'
+  headTag.appendChild(linkforCSSfile);
+  document.body.appendChild(headTag);
+  var z = "dark.css"
+  var check = check + 1
+};
